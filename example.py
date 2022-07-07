412f4940ec2781084e18802693e41fbeb0f5f6c7
haednodik <haednodik@gmail.com>
2022-07-07 00:28:26 -0400

    Add example

---
+++ example.py
@@ -0,0 +1,8 @@
+def game_instruction():
+    print("""Wordle is a single player game
+A player has to guess a five letter hidden word
+You have six attempts
+Your Progress Guide "✔❌❌✔➕"
+"✔" Indicates that the letter at that position was guessed correctly
+"➕" indicates that the letter at that position is in the hidden word, but in a different position
+"❌" indicates that the letter at that position is wrong, and isn't in the hidden word   """)
