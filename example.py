4100292ca43b6398aa7c7ea68f1b9cd5824f22c1
haednodik <haednodik@gmail.com>
2022-07-08 01:00:59 -0400

    Add game logic

--- example.py
+++ example.py
@@ -1,8 +1,29 @@
 def game_instruction():
-    print("""Wordle is a single player game
-A player has to guess a five letter hidden word
-You have six attempts
-Your Progress Guide "✔❌❌✔➕"
-"✔" Indicates that the letter at that position was guessed correctly
-"➕" indicates that the letter at that position is in the hidden word, but in a different position
-"❌" indicates that the letter at that position is wrong, and isn't in the hidden word   """)
+    print("Wordle is a single player game")
+    print("A player has to guess a five letter hidden word")
+    print("You have six attempts")
+    print('Your Progress Guide "✔❌❌✔➕"')
+    print('"✔" Indicates that the letter at that position was guessed correctly')
+    print('"➕" indicates that the letter at that position is in the hidden word, but in a different position')
+    print('''"❌" indicates that the letter at that position is wrong, and isn't in the hidden word''')
+
+def check_word():
+    hidden_word = "snail"
+    attempt = 6
+    while attempt > 0:
+        guess = str(input("Guess the word: "))
+        if guess == hidden_word:
+            print("You guessed the words correctly! WIN 🕺🕺🕺 ")
+            break
+        else:
+            attempt = attempt - 1
+            print(f"You have {attempt} attempt(s) left...")
+            for char, g_char in zip(hidden_word, guess):
+                if g_char in hidden_word and g_char in char:
+                    print(g_char + " ✔ ")
+                elif g_char in hidden_word:
+                    print(g_char + " ➕ ")
+                else:
+                    print(" ❌ ")
+            if attempt == 0:
+                print(" Game over !!!! ")
